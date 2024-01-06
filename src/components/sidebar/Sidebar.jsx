e97d5019541bdf39d4af626e7d109ac208371a74
Hugo Tamm <hugotamm@hotmail.com>
2024-01-07 00:00:49 +0200

    Sidebar good state

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,65 @@
+import './Sidebar.scss';
+import { Icon } from '@iconify/react';
+import logoImage from '../../assets/ht-logo-white.png';
+
+function Sidebar(props) {
+
+    const {open, setOpen} = props;
+
+    return (
+        <div className={'sidebar ' + (open ? 'open' : '')}>
+            <div className="sidebar-logo">
+                <img id="side-logo" src={logoImage} alt="logo" />
+                <h2 id="sidebar-name">Hugo Tamm</h2>
+                <h3 id="sidebar-url">huxystudios.com</h3>
+            </div>
+
+            <div>
+                <div id="side-button" onClick={() => setOpen(!open)}>
+                    <Icon id="side-button-icon" icon="material-symbols:chevron-right-rounded" />
+                </div>
+            </div>
+            
+            <h3 className="sidebar-section">Navigation</h3>
+            <div>
+                <div className="sidebar-icons">
+                    <Icon icon="mdi:user-circle-outline" />
+                    <h3 className="icon-text">About</h3>
+                </div>
+                <div className="sidebar-icons">
+                    <Icon icon="ic:baseline-work-outline" />
+                    <h3 className="icon-text">Work</h3>
+                </div>
+                <div className="sidebar-icons">
+                    <Icon icon="carbon:skill-level-intermediate" />
+                    <h3 className="icon-text">Skills</h3>
+                </div>
+                <div className="sidebar-icons">
+                    <Icon icon="ic:outline-email" />
+                    <h3 className="icon-text">Contact</h3>
+                </div>
+            </div>
+
+            <div id="sectionDivider">
+            </div>
+
+            <h3 className="sidebar-section">Socials</h3>
+            <div>
+                <div className="sidebar-media">
+                    <Icon icon="ph:linkedin-logo-bold" />
+                    <h3 className="icon-text">LinkedIn</h3>
+                </div>
+                <div className="sidebar-media">
+                    <Icon icon="codicon:github" />
+                    <h3 className="icon-text">GitHub</h3>
+                </div>
+            </div>
+
+            <div id="sidebar-copyright">
+                <p>© 2022 Hugo Tamm. All Rights Reserved.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
