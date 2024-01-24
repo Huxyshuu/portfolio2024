ceacc2caddd09ff04b3ba93e767d3fcd05b614b2
Hugo Tamm <hugotamm@hotmail.com>
2024-01-24 20:04:26 +0200

    Contac form test

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -4,15 +4,14 @@ import './ContactForm.scss'
 export default function ContactForm() {
   return (
     <div>
-        <form id="contact-form" action="" method="POST">
-            <input type="hidden" name="access_key" value="8d741132-4400-4c09-a235-9f1a65a25beb"/>
+        <form id="contact-form" action="https://formsubmit.co/el/38c4dba6f8061f9b985b9c6d425ea049" method="POST">
             <div id="contact-sender-info">
               <input id="contact-name" type="text" name="Name" placeholder="Name" required/>
-              <input id="contact-email" type="email" name="Email" placeholder="Email" required/>
+              <input id="contact-email" type="email" name="Email" placeholder="Email (optional)"/>
             </div>
             <textarea name="Message" id="contactMessage" placeholder="Message" 
             cols="30" rows="10" required></textarea>
-            <button>Send (out of order)</button>
+            <button type="submit">Send</button>
         </form>
     </div>
   )
